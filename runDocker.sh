a1ef6a9f728adcb81dac8633b921ad2af70dd602
Cesar Gonzalez <cesar.e.gonzalez@gmail.com>
2017-01-04 18:27:48 -0600

    other

--- runDocker.sh
+++ runDocker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# docker run -p 8080:8080 -e "APP_PROFILE=dev" -e "APP_PASSWORD=user" -e "DB_PASSWORD=Welcome1" -e "DB_USER=sod_user" -it interactivelabs/services:dev
-# docker run -p 8080:8080 -e "APP_PROFILE=local" -e "APP_PASSWORD=user" -e "DB_PASSWORD=Welcome1" -e "DB_USER=root" -it interactivelabs/services:dev
-docker run -p 8080:8080 -e "APP_PROFILE=docker" -e "APP_PASSWORD=user" -e "DB_PASSWORD=Welcome1" -e "DB_USER=sod_user" -it interactivelabs/services:dev
+docker run -p 8080:8080 -e "APP_PROFILE=local" -e "APP_PASSWORD=user" -e "DB_PASSWORD=Welcome1" -e "DB_USER=root" \
+  -it interactivelabs/services:dev
