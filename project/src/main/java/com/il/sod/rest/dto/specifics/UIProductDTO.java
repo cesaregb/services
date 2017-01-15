40c7261727981edbccad3d8927b3702ce607cabc
Cesar Gonzalez <cesar.e.gonzalez@gmail.com>
2017-01-15 13:40:09 -0600

    clean code

--- project/src/main/java/com/il/sod/rest/dto/specifics/UIProductDTO.java
+++ project/src/main/java/com/il/sod/rest/dto/specifics/UIProductDTO.java
@@ -8,23 +8,18 @@ public class UIProductDTO {
 	public int getIdProduct() {
 		return idProduct;
 	}
-
 	public void setIdProduct(int idProduct) {
 		this.idProduct = idProduct;
 	}
-
 	public int getQuantity() {
 		return quantity;
 	}
-
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
-
 	public float getPrice() {
 		return price;
 	}
-
 	public void setPrice(float price) {
 		this.price = price;
 	}
